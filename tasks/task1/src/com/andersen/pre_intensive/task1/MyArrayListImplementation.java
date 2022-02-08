d7c68f5e652a4a13c5b8bce787183384b38649e4
Marat Shigapov <marat@fakeemail.com>
2022-02-08 21:56:06 +0300

    added class implements MyArrrayList

---
+++ tasks/task1/src/com/andersen/pre_intensive/task1/MyArrayListImplementation.java
@@ -0,0 +1,41 @@
+package com.andersen.pre_intensive.task1;
+
+import java.util.ArrayList;
+
+public class MyArrayListImplementation implements MyArrayList {
+
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public void add(Object o, int index) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void concat(MyArrayList newList) {
+
+    }
+
+    @Override
+    public boolean delete(int index) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
