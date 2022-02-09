1a033fa0fce442fb841cc005566a126756dcbca2
pc\One <RomashkaV com1x@rambler.ru>
2022-02-09 13:01:44 +0300

    Добавлен тестовый комментарий с полями: internalArray size startSize
    и методами:
    MyTestListImpl()
    isEmpty()
    growInternalArray(int newElementsCount)

--- tasks/task1/src/com/andersen/pre_intensive/task1/MyArrayListImplementation.java
+++ tasks/task1/src/com/andersen/pre_intensive/task1/MyArrayListImplementation.java
@@ -3,6 +3,38 @@ package com.andersen.pre_intensive.task1;
 import java.util.ArrayList;
 
 public class MyArrayListImplementation implements MyArrayList {
+    /*  предлагаю начать с этого:
+
+
+        private Object[] internalArray;
+        private int size = 0;
+        private int startSize = 10;
+
+    public MyTestListImpl() {
+        Object[] newArray = new Object[startSize];
+        this.internalArray = newArray;
+    }
+    public boolean isEmpty(){
+        if (size == 0) return true;
+        else return false;
+    }
+
+    private void growInternalArray(int newElementsCount){
+        if (isEmpty()) {
+            Object newInternalArray[] = new Object[this.startSize];
+
+        } else {
+            int newSize = size() + newElementsCount;  //
+            Object newInternalArray[] = new Object[newSize];
+            newInternalArray = Arrays.copyOfRange(internalArray, 0, size());
+            this.internalArray = newInternalArray;
+        }
+    }
+
+    private void growInternalArray(){
+        growInternalArray(1);
+    }
+     */
 
     @Override
     public void add(Object o) {
