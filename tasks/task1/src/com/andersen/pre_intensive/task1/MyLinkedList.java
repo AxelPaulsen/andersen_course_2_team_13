440b52b3d2937d1d6cc3bfce03ce61e639a23eae
home <home@DESKTOP-N0GUOBR>
2022-02-13 01:34:53 +0300

    Сделал метод add - добавление в конец списка

--- tasks/task1/src/com/andersen/pre_intensive/task1/MyLinkedList.java
+++ tasks/task1/src/com/andersen/pre_intensive/task1/MyLinkedList.java
@@ -2,31 +2,63 @@ package com.andersen.pre_intensive.task1;
 
 public class MyLinkedList<T> implements MyArrayList<T> {
 
-    private Node<T> firstNode;
+    int size = 0;
+	private Node<T> firstNode;
     private Node<T> lastNode;
 
     public MyLinkedList() {
     }
+    
+    public Node<T> getFirstNode() {
+		return firstNode;
+	}
 
-    private static class Node<T> {
+	public Node<T> getLastNode() {
+		return lastNode;
+	}
+
+	private static class Node<T> {
         T value;
-        MyLinkedList.Node<T> next;
-        MyLinkedList.Node<T> prev;
+        Node<T> next;
+        Node<T> prev;
 
         public Node(T value, Node<T> next, Node<T> prev) {
             this.value = value;
             this.next = next;
             this.prev = prev;
         }
+
+		@Override
+		public String toString() {
+			return "Node [value=" + value + "]";
+		}
     }
 
     @Override
+    // Добавление элемента в конец списка
     public void add(Object o) {
+    	
+    	Node<T> nod;
+    	if (size == 0) {
+    		nod = new Node((T) o, null, null);
+    		firstNode = nod;
+    		lastNode = nod;
+    		System.out.println(firstNode + " " + lastNode);
+    		size ++;
+    	}
+    	else {
+    		nod = new Node((T) o, null, lastNode);
+    		lastNode.next = nod;
+    		lastNode = nod;
+    		size ++;
+    	}
 
     }
 
     @Override
     public void add(Object o, int index) {
+    	
+    	
 
     }
 
