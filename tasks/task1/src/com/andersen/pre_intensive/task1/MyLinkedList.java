9becf93f001b873c09cf9801a1d45f50e47e56c3
home <home@DESKTOP-N0GUOBR>
2022-02-13 18:01:54 +0300

    Сделал add(Object o, int index) Сделал size() Дополнительно создал методы для вывода в консоль объектов Node (toString in Node class) и содержимого списка (метод printOut ()). методы выводят поле value у элемента спсика

--- tasks/task1/src/com/andersen/pre_intensive/task1/MyLinkedList.java
+++ tasks/task1/src/com/andersen/pre_intensive/task1/MyLinkedList.java
@@ -2,7 +2,7 @@ package com.andersen.pre_intensive.task1;
 
 public class MyLinkedList<T> implements MyArrayList<T> {
 
-    int size = 0;
+    int listSize = 0;
 	private Node<T> firstNode;
     private Node<T> lastNode;
 
@@ -16,6 +16,20 @@ public class MyLinkedList<T> implements MyArrayList<T> {
 	public Node<T> getLastNode() {
 		return lastNode;
 	}
+	
+	// Method for print MyLinkedList elements to console
+	public void printOut () {
+		
+		Node<T> element = firstNode;
+		System.out.print(element + " ");
+		
+		for (int i = 0; i < listSize-1; i++) {
+			element = element.next;
+			System.out.print(element + " ");
+		}
+		System.out.println();
+		
+	}
 
 	private static class Node<T> {
         T value;
@@ -28,38 +42,60 @@ public class MyLinkedList<T> implements MyArrayList<T> {
             this.prev = prev;
         }
 
-		@Override
+		// toString for Node class
+        @Override
 		public String toString() {
 			return "Node [value=" + value + "]";
 		}
     }
 
     @Override
-    // Добавление элемента в конец списка
+    // Add element to end of list
     public void add(Object o) {
     	
     	Node<T> nod;
-    	if (size == 0) {
+    	if (listSize == 0) {
     		nod = new Node((T) o, null, null);
     		firstNode = nod;
     		lastNode = nod;
-    		System.out.println(firstNode + " " + lastNode);
-    		size ++;
+    		listSize ++;
     	}
     	else {
     		nod = new Node((T) o, null, lastNode);
     		lastNode.next = nod;
     		lastNode = nod;
-    		size ++;
+    		listSize ++;
     	}
 
     }
 
+   // Add element "o" to list before element with number "index"
     @Override
     public void add(Object o, int index) {
     	
+    	if (index > listSize-1) {
+    		throw new IndexOutOfBoundsException();
+    	}
     	
-
+    	if (index == listSize-1) {
+    		this.add(o);
+    	}
+    	
+    	if (index < listSize-1) {
+    		System.out.println("вставляем перед " + index);
+    		Node<T> replaceElement = firstNode;
+    		for (int i = 0; i < index; i++) {
+    			replaceElement = replaceElement.next;
+    		}
+    		
+    		Node<T> newElement = new Node<> ((T) o, replaceElement, replaceElement.prev);
+    		replaceElement.prev.next = newElement;
+    		replaceElement.prev = newElement;
+    		listSize ++;
+    	
+    	
+    	}
+		
     }
 
     @Override
@@ -82,8 +118,9 @@ public class MyLinkedList<T> implements MyArrayList<T> {
         return null;
     }
 
+    // Method return the list size
     @Override
     public int size() {
-        return 0;
+        return listSize;
     }
 }
